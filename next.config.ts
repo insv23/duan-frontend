4b15ceefb51202dda7133fab1d81488aa9acce42
insv <insv23@outlook.com>
2025-06-22 18:44:29 +0800

    ✨ feat(open-next): add cloudflare pages deployment
    - add configuration files for cloudflare pages deployment
    - add script commands for building and deploying the project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
 
 export default nextConfig;
+
+initOpenNextCloudflareForDev();
